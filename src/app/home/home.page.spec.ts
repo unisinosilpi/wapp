2d8923c17194cb8423db456b97b4f09cd53a18fb
André Treib <andre.schumacher.treib@gmail.com>
2021-09-21 22:36:06 -0300

    clean up project

--- src/app/home/home.page.spec.ts
+++ src/app/home/home.page.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
-import { MessageComponentModule } from '../message/message.module';
 
 import { HomePage } from './home.page';
 
@@ -12,7 +11,7 @@ describe('HomePage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot(), MessageComponentModule, RouterModule.forRoot([])]
+      imports: [IonicModule.forRoot(), RouterModule.forRoot([])]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
